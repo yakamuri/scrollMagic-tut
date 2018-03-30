221044aab821660b9fab6075620b4e20a2a88a45
szucst <t.szucs@plan-net.com>
2018-03-30 13:42:49 +0300

    setup

---
+++ static/_first_tut.php
@@ -0,0 +1,38 @@
+
+<section id="one">
+    <div class="inner innerS1">
+        <h1>Simple ScrollMagic Tutorial</h1>
+        <p>Learn how to create a <strong>simple scrolling website</strong> using ScrollMagic</p>
+    </div>
+</section>
+<section id="two">
+    <div class="innerText innerS2">
+        <h2>Why to learn ScrollMagic?</h2>
+        <ul class="features">
+            <li>It's great for <strong>story telling websites</strong></li>
+            <li>It gives you <strong>endless creative power</strong></li>
+            <li>It's <strong>easy to use</strong> once you get it</li>
+        </ul>
+    </div>
+</section>
+<section id="three">
+    <div class="innerText innerS3">
+        <h2>When to use ScrollMagic?</h2>
+        <ul class="features">
+            <li>When building <strong>interactive infographics</strong></li>
+            <li>When introducing your <strong>product or service</strong></li>
+            <li>When sharing your <strong>unique story or timeline</strong></li>
+        </ul>
+    </div>
+</section>
+<section id="four">
+    <div class="innerText innerS4">
+        <h2>Want to learn more?</h2>
+    </div>
+    <div class="iphone">
+        <p class="screen screenHat"><img src="assets/images/img_scrollMagic.png" /></p>
+        <p class="screen screenA">A</p>
+        <p class="screen screenB">B</p>
+        <p class="screen screenC">C</p>
+    </div>
+</section>
--- static/assets/Gruntfile.js
+++ static/assets/Gruntfile.js
@@ -33,7 +33,8 @@ module.exports = function(grunt) {
             custom: {
                 files: {
                     'js/app.min.js': [
-                        'js/toMinify/_main.js'
+                        'js/toMinify/_main_first.js'
+                        //'js/toMinify/_main_second.js'
                     ]
                 }
             },
@@ -41,7 +42,11 @@ module.exports = function(grunt) {
                 files: {
                     'js/vendor.min.js': [
                         'js/toMinify/vendor/jquery.min.js',
-                        'js/toMinify/vendor/modernizr.custom.92950.js'
+                        'js/toMinify/vendor/modernizr.custom.92950.js',
+                        'js/toMinify/vendor/TweenMax.min.js',
+                        'js/toMinify/vendor/scrollMagic.js',
+                        'js/toMinify/vendor/animation.gsap.min.js',
+                        'js/toMinify/vendor/debug.addIndicators.min.js'
                     ]
                 }
             }
